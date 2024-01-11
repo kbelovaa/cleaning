df798321eee73d98b7723237b543bdaa5de96c71
Ekaterina Belova <kbelovaa17@gmail.com>
2024-01-11 17:29:28 +0300

    Commit

--- src/components/PhoneField/PhoneField.jsx
+++ src/components/PhoneField/PhoneField.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import PhoneInput from 'react-phone-input-2';
 import { isValidNumber } from 'libphonenumber-js';
 import 'react-phone-input-2/lib/style.css';
@@ -7,6 +8,8 @@ import './PhoneField.scss';
 
 const PhoneField = ({ mobile, setMobile, isMobileValid, setIsMobileValid }) => {
   const ipCountry = useSelector((state) => state.user.ipCountry);
+
+  const { t } = useTranslation();
 
   const handleMobileChange = (value, country) => {
     setMobile(value);
@@ -19,7 +22,7 @@ const PhoneField = ({ mobile, setMobile, isMobileValid, setIsMobileValid }) => {
   return (
     <div className="phone-field">
       <label htmlFor="mobile" className="form__label">
-        Mobile nr
+        {t('mobile')}
       </label>
       <PhoneInput
         value={mobile}
@@ -30,7 +33,7 @@ const PhoneField = ({ mobile, setMobile, isMobileValid, setIsMobileValid }) => {
         buttonClass={`${!ipCountry && !mobile ? 'hidden' : ''} ${!mobile || !isMobileValid ? 'invalid-field' : ''}`}
         autoFormat
       />
-      <p className={isMobileValid ? 'hidden' : 'auth__note'}>Please enter a valid mobile number.</p>
+      <p className={isMobileValid ? 'hidden' : 'auth__note'}>{t('validMobileMessage')}</p>
     </div>
   );
 };
