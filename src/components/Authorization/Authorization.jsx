20b3dd0598e290d69f226591c4c36ebf8cd5c308
Ekaterina Belova <kbelovaa17@gmail.com>
2024-01-16 21:02:20 +0300

    Commit

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -172,17 +172,9 @@ const Authorization = ({ isOpen, setIsOpen, isLogin, setIsLogin }) => {
     <>
       <div className={`modal ${isOpen ? 'active' : ''}`}>
         <div className={`auth ${isLogin ? 'login' : 'reg'}`} ref={modalRef}>
-          <svg
-            className="auth__close"
-            onClick={closeModal}
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-          >
-            <path d="M23.5425 23.5424L8.45758 8.45746" stroke="#268664" strokeLinecap="round" />
-            <path d="M23.5424 8.45746L8.45747 23.5424" stroke="#268664" strokeLinecap="round" />
+          <svg className="auth__close" onClick={closeModal} xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
+            <path d="M22.0712 22.0709L7.9291 7.92881" stroke="#268664" strokeLinecap="round"/>
+            <path d="M22.0709 7.92881L7.92876 22.0709" stroke="#268664" stroke-Linecap="round"/>
           </svg>
           <h2 className="auth__title">{isLogin ? t('logIn') : t('signUp')}</h2>
           <form className={`form auth-form ${isFormValid ? 'valid' : 'invalid'}`} onSubmit={handleFormSubmit}>
