f8af5b99d63cafd56a65fae451886584b95cdaa5
Ekaterina Belova <kbelovaa17@gmail.com>
2024-07-12 15:28:02 +0300

    Commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -137,7 +137,6 @@ const App = () => {
           <Route path="work/survey" element={<Survey />} />
           <Route path="work/instructions" element={<Instructions />} />
           <Route path="affiliate-program" element={<Affiliates />} />
-          <Route path="error" element={<ErrorPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
